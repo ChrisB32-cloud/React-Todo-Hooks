5d2f077ce2f3aa854cf21bd59aa564aa0e7d885e
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-01-07 09:28:35 -0600

    Imported List Divider and ListItem from material ui

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
 
 const TodoList = ({ task }) => {
   console.log(task);
   return (
     <Paper>
-      <h1>Todo List</h1>
-      <ul>
+      <List>
         {task.map(m => (
           <li key={m.id}>
             {m.task} {m.completed}{' '}
           </li>
         ))}
-      </ul>
+      </List>
     </Paper>
   );
 };
